df8b52e8eb9e0ca19d21508f3d8b068648c21c8a
aayush-meshram <aayushmeshram007@gmail.com>
2025-02-04 23:02:24 +0530

    Initial setup

---
+++ src/main/java/com/example/tfpsql/TfpsqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.tfpsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfpsqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfpsqlApplication.class, args);
+	}
+
+}
